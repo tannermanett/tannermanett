d70553bf85fa72e7527c7ac4899af2c9e6cf6620
tannermanett <102496928+tannermanett@users.noreply.github.com>
2022-05-01 22:36:15 -0400

    Add files via upload

---
+++ Analyze_Crypto.py
@@ -0,0 +1,41 @@
+import pandas_datareader as web
+import matplotlib.pyplot as plt
+import mplfinance as mpf
+import seaborn as sns
+import datetime as dt
+
+currency = "USD"
+metric = "Close"
+
+start = dt.datetime(2018,1,1)
+end = dt.datetime.now()
+
+crypto = ['BTC', 'ETH', 'ADA', 'SOL', 'AVAX', 'BADGER']
+colnames = []
+
+first = True
+
+for ticker in crypto:
+    data = web.DataReader(f'{ticker}-{currency}', "yahoo", start, end)
+    if first:
+        combined = data[[metric]].copy()
+        colnames.append(ticker)
+        combined.columns = colnames
+        first = False
+    else:
+        combined = combined.join(data[metric])
+        colnames.append(ticker)
+        combined.columns = colnames
+
+#plot crypto movement
+#plt.yscale('log')
+#for ticker in crypto:
+    #plt.plot(combined[ticker], label=ticker)
+#plt.legend(loc="upper right")
+
+#plot correlation heatmap
+combined = combined.pct_change().corr(method="pearson")
+
+sns.heatmap(combined, annot=True, cmap="coolwarm")
+plt.show()
+
